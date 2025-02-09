f63bdd9ef7078cb7a5edff20c12fe084cc8e2c37
Tanmay Raj Krishna <tanmayrajkrishna@gmail.com>
2025-02-09 13:49:27 +0530

    add headers

--- scrape.py
+++ scrape.py
@@ -6,6 +6,9 @@ def extract_cheapest_price(dep_airport, arr_airport):
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
+        page.set_extra_http_headers({
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        })
         base_url = "https://www.google.com/travel/flights/search"
         params = "?tfs=" + get_b64_encoded_tfs(dep_airport, arr_airport) + "&hl=en&curr=usd"
         full_url = base_url + params
