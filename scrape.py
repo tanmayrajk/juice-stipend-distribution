b2e16285435f681b5da083b76ad4a7928bfa0f5e
Tanmay Raj Krishna <tanmayrajkrishna@gmail.com>
2025-02-09 16:16:35 +0530

    try 5

--- scrape.py
+++ scrape.py
@@ -4,15 +4,11 @@ import re
 import json
 
 def extract_cheapest_price(dep_airport, arr_airport):
-    with open("cookies.json", "r") as f:
-        cookies = json.load(f)
-
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         context = browser.new_context(
             viewport={"width": 1280, "height": 720}
         )
-        context.add_cookies(cookies)
         page = context.new_page()
         page.set_extra_http_headers({
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
@@ -23,9 +19,9 @@ def extract_cheapest_price(dep_airport, arr_airport):
         page.goto(full_url)
         page.wait_for_load_state('networkidle')
         cheapest_card_text = page.wait_for_selector("div#M7sBEb", timeout=2000).inner_text()
-        cookies = page.context.cookies()
-        # with open("cookies.json", "w") as f:
-        #     json.dump(cookies, f)
+        html_content = page.content()
+        with open("page.html", "w", encoding="utf-8") as file:
+            file.write(html_content)
         browser.close()
 
         match = re.search(r"\$\d{1,3}(?:,\d{3})*", cheapest_card_text)
